b1d86d43daf97d871bccbcf80b354cf9484bc438
liuhongxu <liuhongxu@MBP-2F92MJMD6P-0008.local>
2023-07-06 19:21:53 +0800

    log

---
+++ bisearch.py
@@ -0,0 +1,45 @@
+def bisearch(arr: list, target) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while(left <= right):
+        mid = left + (right - left) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] == target:
+            return mid
+    
+    return -1
+
+def bisearchL(arr: list, target) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while(left <= right):
+        mid = left + (right - left) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] == target:
+            right = mid - 1
+    
+    if left >= len(arr):
+        return -1
+    return left if arr[left]==target else -1
+
+def bisearchR(arr: list, target) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while(left <= right):
+        mid = left + (right - left) // 2
+        if arr[mid] > target:
+            right = mid - 1
+        elif arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] == target:
+            left = mid - 1
+    
+    if right < 0:
+        return -1
+    return right if arr[right]==target else -1
